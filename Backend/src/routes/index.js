0eb8eb4568e234efb2b588a7083db6c4f9111272
pxxja <poojabhambid2004@gmail.com>
2026-01-21 22:24:08 +0530

    add entry point for ticket apis

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth.route.js";
+import ticketRouter from "./ticket.route.js";
 
 const mainRouter=Router();
 
 
 mainRouter.use('/auth',authRouter)
+mainRouter.use('/ticket',ticketRouter)
 
 export default mainRouter
