d3c32adfd8e7afc5957414254b3362e93cc6e55b
pxxja <poojabhambid2004@gmail.com>
2026-01-21 15:50:44 +0530

    update folder name

--- Backend/src/config/cloudinary.js
+++ Backend/src/config/cloudinary.js
@@ -12,7 +12,7 @@ cloudinary.config({
 export const uploadToCloudinary = async (filePath) => {
   try {
     const result = await cloudinary.uploader.upload(filePath, {
-      folder: "ticket/products", // change to your own folder
+      folder: "ticket/tickets", // change to your own folder
     });
 
     return {
