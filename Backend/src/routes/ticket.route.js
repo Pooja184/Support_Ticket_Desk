9e610f76f1705a05e5d54fac62515360acdf45f2
pxxja <poojabhambid2004@gmail.com>
2026-01-22 14:14:38 +0530

    build api for attachments

--- Backend/src/routes/ticket.route.js
+++ Backend/src/routes/ticket.route.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { addTicket } from "../controllers/ticket.controller.js";
+import { addTicket, addTicketAttachment } from "../controllers/ticket.controller.js";
 import { tokenDecoder } from "../middlewares/tokenDecoder.js";
+import { upload } from "../config/multer.js";
 
+const ticketRouter = Router();
 
-const ticketRouter=Router();
-
-ticketRouter.post('/add-ticket',tokenDecoder,addTicket)
-
-export default ticketRouter
+ticketRouter.post("/add-ticket", tokenDecoder, addTicket);
+ticketRouter.post(
+  "/:id/attachments",
+  tokenDecoder,
+  upload.single("file"),
+  addTicketAttachment,
+);
+export default ticketRouter;
