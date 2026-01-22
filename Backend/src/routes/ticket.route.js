7d8492d5c98d26d0bf3de38d515863e83ac2dcd0
pxxja <poojabhambid2004@gmail.com>
2026-01-22 14:56:48 +0530

    add apis for comments

--- Backend/src/routes/ticket.route.js
+++ Backend/src/routes/ticket.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addTicket, addTicketAttachment, getMyTickets } from "../controllers/ticket.controller.js";
+import { addComment, addTicket, addTicketAttachment, getComments, getMyTickets } from "../controllers/ticket.controller.js";
 import { tokenDecoder } from "../middlewares/tokenDecoder.js";
 import { upload } from "../config/multer.js";
 
@@ -14,4 +14,8 @@ ticketRouter.post(
 );
 
 ticketRouter.get("/my-tickets",tokenDecoder,getMyTickets)
+
+ticketRouter.post("/:id/comments",tokenDecoder,addComment)
+ticketRouter.get("/:id/comments",tokenDecoder,getComments)
+
 export default ticketRouter;
