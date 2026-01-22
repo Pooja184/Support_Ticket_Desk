40d7a081938dd61dc62f011f0353664c1c785d7c
pxxja <poojabhambid2004@gmail.com>
2026-01-22 13:43:51 +0530

    add routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { Route, Routes } from "react-router-dom"
+import Welcome from "./pages/Welcome"
+import Register from "./pages/user/Register"
+import Login from "./pages/user/Login"
+import Dashboard from "./pages/user/Dashboard"
+import AddTicket from "./pages/user/AddTicket"
 
 
 function App() {
 
   return (
    <>
-   <h1 style={{backgroundColor:"red"}}>helllo</h1>
+   <Routes>
+    <Route path="/" element={<Welcome/>}/>
+    <Route path="/user-register" element={<Register/>}/>
+    <Route path="/user-login" element={<Login/>}/>
+
+    <Route path="/dashboard" element={<Dashboard/>}>
+        <Route path="add-tickets" element={<AddTicket/>}/>
+    </Route>
+   </Routes>
    </>
   )
 }
