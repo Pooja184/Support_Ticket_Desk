ccc74dd2c7d5f2a62acec5e355e32cef77018457
pxxja <poojabhambid2004@gmail.com>
2026-01-22 23:02:52 +0530

    changes in api end points

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,36 @@
-import { Route, Routes } from "react-router-dom"
-import Welcome from "./pages/Welcome"
-import Register from "./pages/user/Register"
-import Login from "./pages/user/Login"
-import Dashboard from "./pages/user/Dashboard"
-import AddTicket from "./pages/user/AddTicket"
-import MyTickets from "./pages/user/MyTickets"
-import TicketDetails from "./pages/user/TicketDetails"
-
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Register from "./pages/user/Register";
+import Login from "./pages/user/Login";
+import Dashboard from "./pages/user/Dashboard";
+import AddTicket from "./pages/user/AddTicket";
+import MyTickets from "./pages/user/MyTickets";
+import TicketDetails from "./pages/user/TicketDetails";
+import AdminLogin from "./pages/admin/AdminLogin";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AllTickets from "./pages/admin/AllTickets";
 
 function App() {
-
   return (
-   <>
-   <Routes>
-    <Route path="/" element={<Welcome/>}/>
-    <Route path="/user-register" element={<Register/>}/>
-    <Route path="/user-login" element={<Login/>}/>
+    <>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/user-register" element={<Register />} />
+        <Route path="/user-login" element={<Login />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
 
-    <Route path="/dashboard" element={<Dashboard/>}>
-        <Route path="/dashboard/add-tickets" element={<AddTicket/>}/>
-        <Route path="/dashboard/my-tickets" element={<MyTickets/>}/>
-<Route path="tickets/:id" element={<TicketDetails />} />
-    </Route>
-   </Routes>
-   </>
-  )
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="add-tickets" element={<AddTicket />} />
+          <Route path="my-tickets" element={<MyTickets />} />
+          <Route path="tickets/:id" element={<TicketDetails />} />
+        </Route>
+
+        <Route path="/admin-dashboard" element={<AdminDashboard/>}>
+          <Route path="all-tickets" element={<AllTickets/>}/>
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
