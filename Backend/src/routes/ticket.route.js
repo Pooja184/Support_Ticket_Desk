73c0d7e2d7cf0a37edcd598b4c801c3c310c14e4
pxxja <poojabhambid2004@gmail.com>
2026-01-22 15:17:49 +0530

    defined api route for getTicketById

--- Backend/src/routes/ticket.route.js
+++ Backend/src/routes/ticket.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addComment, addTicket, addTicketAttachment, getComments, getMyTickets } from "../controllers/ticket.controller.js";
+import { addComment, addTicket, addTicketAttachment, getComments, getMyTickets, getTicketById } from "../controllers/ticket.controller.js";
 import { tokenDecoder } from "../middlewares/tokenDecoder.js";
 import { upload } from "../config/multer.js";
 
@@ -17,5 +17,7 @@ ticketRouter.get("/my-tickets",tokenDecoder,getMyTickets)
 
 ticketRouter.post("/:id/comments",tokenDecoder,addComment)
 ticketRouter.get("/:id/comments",tokenDecoder,getComments)
+
+ticketRouter.get("/:id",tokenDecoder,getTicketById)
 
 export default ticketRouter;
