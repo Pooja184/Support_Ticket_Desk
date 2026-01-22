bd8e13fd9fe878dea9ff62425fea2902d18b1864
pxxja <poojabhambid2004@gmail.com>
2026-01-22 22:26:20 +0530

    add routes for update and all tickets

--- Backend/src/routes/ticket.route.js
+++ Backend/src/routes/ticket.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addComment, addTicket, addTicketAttachment, getComments, getMyTickets, getTicketById } from "../controllers/ticket.controller.js";
+import { addComment, addTicket, addTicketAttachment, getAllTickets, getComments, getMyTickets, getTicketById, updateTicketStatus } from "../controllers/ticket.controller.js";
 import { tokenDecoder } from "../middlewares/tokenDecoder.js";
 import { upload } from "../config/multer.js";
 
@@ -15,9 +15,13 @@ ticketRouter.post(
 
 ticketRouter.get("/my-tickets",tokenDecoder,getMyTickets)
 
+ticketRouter.get("/all-tickets",tokenDecoder,getAllTickets)
 ticketRouter.post("/:id/comments",tokenDecoder,addComment)
 ticketRouter.get("/:id/comments",tokenDecoder,getComments)
 
 ticketRouter.get("/:id",tokenDecoder,getTicketById)
+
+ticketRouter.patch("/:id/status",tokenDecoder,updateTicketStatus)
+
 
 export default ticketRouter;
